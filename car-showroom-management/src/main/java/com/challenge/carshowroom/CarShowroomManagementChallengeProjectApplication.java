5595730db1026bd35d628041ec9fe9855a18340f
SamiEnazi <v.ipxc46540@gmail.com>
2025-01-09 20:15:27 +0000

    Initial Commit

---
+++ car-showroom-management/src/main/java/com/challenge/carshowroom/CarShowroomManagementChallengeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.carshowroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShowroomManagementChallengeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarShowroomManagementChallengeProjectApplication.class, args);
+	}
+
+}
